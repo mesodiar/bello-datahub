226de8c401f6285b5b4765c347f49814490187af
root <root@datahub-poc-mils.asia-southeast1-c.c.thanos-dev.internal>
2021-10-13 13:29:56 +0000

    :sparkles: Add dag for config :koala:

---
+++ airflow_install/dags/ingest_bigquery_by_config.py
@@ -0,0 +1,73 @@
+"""MySQL DataHub Ingest DAG
+
+This example demonstrates how to ingest metadata from MySQL into DataHub
+from within an Airflow DAG. Note that the DB connection configuration is
+embedded within the code.
+"""
+
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.models import Variable
+
+
+try:
+    from airflow.operators.python import PythonOperator
+except ModuleNotFoundError:
+    from airflow.operators.python_operator import PythonOperator
+
+from datahub.ingestion.run.pipeline import Pipeline
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email": ["jdoe@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "execution_timeout": timedelta(minutes=120),
+}
+
+
+bigquery_thanos_dev = Variable.get("bigquery_thanos_dev_secret")
+datahub_gms_ip = Variable.get("datahub-gms-ip")
+
+
+def ingest_from_mysql():
+    pipeline = Pipeline.create(
+        # This configuration is analogous to a recipe configuration.
+        {
+            "source": {
+                "type": "bigquery",
+                "config": {
+                    "project_id": "thanos-dev",
+                    "options": {
+                        "credentials_path": bigquery_thanos_dev
+                    },
+                    "env": "DEV"
+                },
+            },
+            "sink": {
+                "type": "datahub-rest",
+                "config": {"server": f"http://{datahub_gms_ip}:8080"},
+            },
+        }
+    )
+    pipeline.run()
+    pipeline.raise_from_status()
+
+
+with DAG(
+    "ingest_bigquery_by_config",
+    default_args=default_args,
+    description="An example DAG which ingests metadata from MySQL to DataHub",
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(2),
+    catchup=False,
+) as dag:
+    ingest_task = PythonOperator(
+        task_id="ingest_from_mysql",
+        python_callable=ingest_from_mysql,
+    )
